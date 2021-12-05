776230df0a0d6501d2371a1a2b847abc76381ec0
Gabby0sorio <90849848+Gabby0sorio@users.noreply.github.com>
2021-12-04 23:13:30 -0500

    fixedcode

--- Assets/scripts/Snake.cs
+++ Assets/scripts/Snake.cs
@@ -1,83 +1,105 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
+[RequireComponent(typeof(BoxCollider2D))]
 public class Snake : MonoBehaviour
 {
-    private Vector2 _direction = Vector2.right;
-
-    private List<Transform> _segments; 
-
+    private List<Transform> _segments = new List<Transform>();
     public Transform segmentPrefab;
-
+    public Vector2 direction = Vector2.right;
     public int initialSize = 4;
-
 
     private void Start()
     {
-        _segments = new List<Transform>();
-        _segments.Add(this.transform);
+        ResetState();
     }
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W)) {
-
-            _direction = Vector2.up;
-        }
-        else if (Input.GetKeyDown(KeyCode.S))
+        // Only allow turning up or down while moving in the x-axis
+        if (this.direction.x != 0f)
         {
-            _direction = Vector2.down;
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                this.direction = Vector2.up;
+            }
+            else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                this.direction = Vector2.down;
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.A))
+        // Only allow turning left or right while moving in the y-axis
+        else if (this.direction.y != 0f)
         {
-            _direction = Vector2.left;
-        }
-        else if (Input.GetKeyDown(KeyCode.D))
-        {
-            _direction = Vector2.right;
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                this.direction = Vector2.right;
+            }
+            else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                this.direction = Vector2.left;
+            }
         }
     }
 
     private void FixedUpdate()
     {
-        for (int i = _segments.Count -1; i > 0; i--)
+        // Set each segment's position to be the same as the one it follows. We
+        // must do this in reverse order so the position is set to the previous
+        // position, otherwise they will all be stacked on top of each other.
+        for (int i = _segments.Count - 1; i > 0; i--)
         {
             _segments[i].position = _segments[i - 1].position;
         }
-        this.transform.position = new Vector3(
-            Mathf.Round(this.transform.position.x) + _direction.x,
-            Mathf.Round(this.transform.position.y) + _direction.y,
-            0.0f
-            );
 
+        // Move the snake in the direction it is facing
+        // Round the values to ensure it aligns to the grid
+        float x = Mathf.Round(this.transform.position.x) + this.direction.x;
+        float y = Mathf.Round(this.transform.position.y) + this.direction.y;
+
+        this.transform.position = new Vector2(x, y);
     }
-    private void Grow()
+
+    public void Grow()
     {
         Transform segment = Instantiate(this.segmentPrefab);
-        segment.position = _segments[ _segments.Count -1].position;
-        _segments.Add(segment);
+        segment.position = _segments[_segments.Count - 1].position;
 
+        _segments.Add(segment);
     }
-    private void ResetState()
+
+    public void ResetState()
     {
-        for (int i = 1; i < _segments.Count; i++) {
+        this.direction = Vector2.right;
+        this.transform.position = Vector3.zero;
+
+        // Start at 1 to skip destroying the head
+        for (int i = 1; i < _segments.Count; i++)
+        {
             Destroy(_segments[i].gameObject);
         }
 
+        // Clear the list but add back this as the head
         _segments.Clear();
         _segments.Add(this.transform);
 
+        // -1 since the head is already in the list
+        for (int i = 0; i < this.initialSize - 1; i++)
+        {
+            Grow();
+        }
     }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Food")
-       {
+        {
             Grow();
-
-        } else if (other.tag == "Obstacle")
+        }
+        else if (other.tag == "Obstacle")
         {
             ResetState();
         }
     }
+
 }
--- Assets/scripts/food.cs
+++ Assets/scripts/food.cs
@@ -2,33 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class food : MonoBehaviour
+public class Food : MonoBehaviour
 {
-    public BoxCollider2D gridArea;
+    public Collider2D gridArea;
 
     private void Start()
     {
         RandomizePosition();
     }
 
-    private void RandomizePosition()
-        {
+    public void RandomizePosition()
+    {
         Bounds bounds = this.gridArea.bounds;
 
+        // Pick a random position inside the bounds
         float x = Random.Range(bounds.min.x, bounds.max.x);
         float y = Random.Range(bounds.min.y, bounds.max.y);
 
-        this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
-  
+        // Round the values to ensure it aligns with the grid
+        x = Mathf.Round(x);
+        y = Mathf.Round(y);
+
+        this.transform.position = new Vector2(x, y);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Player")
-        {
-            RandomizePosition();
-
-        }
+        RandomizePosition();
     }
 }
-
