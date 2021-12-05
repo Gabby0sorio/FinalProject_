a6f51bc64f73d3aead31782bd1a646968b6681e3
Gabby0sorio <90849848+Gabby0sorio@users.noreply.github.com>
2021-12-05 13:08:45 -0500

    hi

--- Assets/scripts/food.cs
+++ Assets/scripts/food.cs
@@ -4,30 +4,27 @@ using UnityEngine;
 
 public class Food : MonoBehaviour
 {
-    public Collider2D gridArea;
+    public BoxCollider2D GridArea;
 
     private void Start()
     {
         RandomizePosition();
     }
 
-    public void RandomizePosition()
+    private void RandomizePosition()
     {
-        Bounds bounds = this.gridArea.bounds;
+        Bounds bounds = this.GridArea.bounds;
 
-        // Pick a random position inside the bounds
+        
         float x = Random.Range(bounds.min.x, bounds.max.x);
         float y = Random.Range(bounds.min.y, bounds.max.y);
-
-        // Round the values to ensure it aligns with the grid
-        x = Mathf.Round(x);
-        y = Mathf.Round(y);
-
-        this.transform.position = new Vector2(x, y);
+        this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.tag == "Player")
         RandomizePosition();
     }
+
 }
